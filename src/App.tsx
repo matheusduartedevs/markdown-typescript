e14633f9e3bf101137a59419566b87f3016f79be
unknown <matheus.duartesilva42@gmail.com>
2023-10-04 20:07:16 -0300

    feat: desenvolvimento da lógica de adicionar do markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from 'react-bootstrap'
+import { useLocalStorage } from './components/useLocalStorage'
 import NewNote from './components/NewNote'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export type Tag = {
+  id: string,
+  label: string
+}
+
+export type NoteData = {
+  title: string,
+  markdown: string,
+  tags: Tag[]
+}
+
+export type Note = {
+  id: string
+} & NoteData
+
+export type RawNote = {
+  id: string
+}
+
+export type RawNoteData = {
+  title: string,
+  markdown: string,
+  tagIds: string[]
+}
 
 const App = () => {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
   return (
     <>
       <Container className='my-4'>
