8c31595533282659306ccf1a2324ed0b718cf969
unknown <matheus.duartesilva42@gmail.com>
2023-10-04 20:33:30 -0300

    feat: logica tag creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+import { useMemo } from 'react'
 import { useLocalStorage } from './components/useLocalStorage'
+import { v4 as uuidV4 } from 'uuid'
 import NewNote from './components/NewNote'
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -21,7 +23,7 @@ export type Note = {
 
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string,
@@ -32,6 +34,21 @@ export type RawNoteData = {
 const App = () => {
   const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
   const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  const onCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes, 
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }
+      ]
+    })
+  }
 
   return (
     <>
